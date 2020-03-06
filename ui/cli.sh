87e0700e06eb5dffdce5d338a3472c44a662e1f3
Arctic Kona <kona@arcticjieer.fam.cx>
2020-03-06 14:37:04 -0500

    temp

--- ui/cli.sh
+++ ui/cli.sh
@@ -1,51 +1,47 @@
 #!/bin/bash
 # Simple User Interface API. Copyright (c) 2020 Arctic Kona. No Rights Reserved.
-import misc/argument
 import misc/default
 import misc/check
 import log/log
 import terminal/font
 
-alias ui_arguments='
-	local ARG_text ARG_title
-	default ARG_text="${ARG_0}"
-	default ARG_title="${ARG_1}"
-'
 alias ui_title='
-	[[ "${ARG_title}" != "" ]] &&
-		echo "${FONT_BOLD}${FONT_LINE}${ARG_title}${FONT_UNLINE}${FONT_UNBOLD}"
-	[[ "${ARG_text}" != "" ]] &&
-		echo "${ARG_text}"
+	[[ "${title}" != "" ]] &&
+		echo "${FONT_BOLD}${FONT_LINE}${title}${FONT_UNLINE}${FONT_UNBOLD}"
+	[[ "${text}" != "" ]] &&
+		echo "${text}"
 '
 
 function ui_info {
-	argument
-	ui_arguments
+	local text title
+	default text="$1"
+	default title="$2"
+
 	ui_title
 
 	return $?
 }
 
 function ui_warn {
-	argument
-	ui_arguments
-	( ui_title ) 1>&2
+	local text
+	default text="$1"
+	default title="$2"
+
+	{ ui_title } 1>&2
 
 	return $?
 }
 
 function ui_text {
-	argument
-	ui_arguments
+	local file
+	default file="$1"
 
 	# Process title and text input
-	local text
-	[[ "${ARG_title}" != "" ]] &&
-		text=">>> ${ARG_title} <<<
-"
-	[[ "${ARG_text}" != "" ]] &&
-		text="$text${ARG_text}" ||
-		text="${text}`cat - `"
+	[[ "${text}" == "" ]] &&
+		text="$( cat $file )"
+	[[ "${title}" == "" ]] ||
+		text=">>> ${title} <<<
+$text"
 
 	# Find an display program
 	for editor in 'less' 'more' 'cat' ; do
@@ -60,47 +56,51 @@ function ui_text {
 }
 
 function ui_entry {
-	argument
-	ui_arguments
 	ui_title
 
 	printf " > "
-	head -n 1 -
+	if [[ "$1" == "" ]] ; then
+		head -n 1 -
+	else
+		$1=$( head -n 1 - )
+	fi
+
 	return $?
 }
 
 function ui_edit {
-	local ARG_text ARG_title
-	argument
-	default ARG_file="${ARG_0}"
-	default ARG_text="${ARG_1}"
-	default ARG_title="${ARG_2}"
-
 	# Provide information to user
-	if [[ "${ARG_title}" != "" ]] || [[ "${ARG_text}" != "" ]] ; then
+	if [[ "${title}" != "" ]] || [[ "${text}" != "" ]] ; then
 		ui_title
 		echo "[ENTER]"
-		head -n 1 -
+		read -n 1 -s
 	fi
 
+
 	# Get STDIN or provided file
-	local content
-	if [[ "${ARG_file}" ]] ; then
-		content="${ARG_file}"
-	else
-		content=$( mktemp ) ||
+	if [[ "${file}" == "" ]] ; then
+		file=$( mktemp ) ||
 			return $?
-		cat - 1> "$content" ||
+		cat - 1> "$file" ||
 			return $?
 	fi
 
 	# Edit!
 	for editor in editor nano vim vi ed ; do
 		if check_command $editor ; then
-			$editor "$content" ||
-				return $?
-			cat "$content"
-			return $?
+			$editor "$file"
+			rtn=$?
+			if [[ -f "$file" ]] ; then
+				if [[ $rtn -eq 0 ]] ; then
+					cat "$file"
+					rtn=$?
+				fi
+				rm "$file"
+				return $rtn
+			fi
+			[[ $rtn -gt 0 ]] &&
+				return $rtn ||
+				return 3
 		fi
 	done
 
@@ -110,12 +110,14 @@ function ui_edit {
 }
 
 function ui_list {
-	argument
-	ui_arguments
 	ui_title
 
+	if 
+	while true ; do
+		
+	done
 
-	log_err "not implemented"
+	return 0
 }
 
 function ui_checkbox {
